e06ae73e7ec76f01aafda5b27f082528a0b74481
Istrate-Mihai <i.mihai9960@gmail.com>
2024-11-09 18:13:22 +0200

    Added images of characters

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
-function getRandomAttackValue(min, max) {
+function getRandomValue(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function getMonsterGallery() {
+  return [
+    'images/monster_1.jpeg',
+    'images/monster_2.jpeg',
+    'images/monster_3.jpeg',
+    'images/monster_4.jpeg',
+    'images/monster_5.jpeg',
+    'images/monster_6.jpeg',
+  ];
+}
+
+function getHeroGallery() {
+  return [
+    'images/hero_1.jpeg',
+    'images/hero_2.jpeg',
+    'images/hero_3.jpeg',
+    'images/hero_4.jpeg',
+    'images/hero_5.jpeg',
+    'images/hero_6.jpeg',
+  ];
 }
 
 const app = Vue.createApp({
@@ -9,6 +31,9 @@ const app = Vue.createApp({
       playerHealth: 100,
       countRound: 0,
       winner: null,
+      logMessageList: [],
+      monsterImageList: getMonsterGallery(),
+      heroImageList: getHeroGallery(),
     };
   },
   watch: {
@@ -50,44 +75,72 @@ const app = Vue.createApp({
     },
     mayUseSpecialAttack() {
       return this.countRound % 3 != 0;
-    }
+    },
+    monsterImage() {
+      const randomIndex = getRandomValue(0, this.monsterImageList.length);
+      return this.monsterImageList[randomIndex];
+    },
+    heroImage() {
+      const randomIndex = getRandomValue(0, this.heroImageList.length);
+      return this.heroImageList[randomIndex];
+    },
   },
   methods: {
     startGame() {
-      this.monsterHealth = 100;
-      this.playerHealth  = 100;
-      this.countRound    = 0;
-      this.winner        = null;
+      this.monsterHealth    = 100;
+      this.playerHealth     = 100;
+      this.countRound       = 0;
+      this.winner           = null;
+      this.logMessageList   = [];
+      this.monsterImageList = getMonsterGallery();
+      this.heroImageList    = getHeroGallery();
     },
     attackMonster() {
-      const attackValue = getRandomAttackValue(5, 12);
+      const attackValue = getRandomValue(5, 12);
       this.monsterHealth -= attackValue;
+      this.addLogMessage('player', 'attack', attackValue);
       this.attackPlayer();
       this.countRound += 1;
     },
     attackPlayer() {
-      const attackValue = getRandomAttackValue(8, 15);
+      const attackValue = getRandomValue(8, 15);
       this.playerHealth -= attackValue;
+      this.addLogMessage('monster', 'attack', attackValue);
     },
     specialAttackMonster() {
-      const attackValue = getRandomAttackValue(10, 25);
+      const attackValue = getRandomValue(10, 25);
       this.monsterHealth -= attackValue;
+      this.addLogMessage('player', 'attack', attackValue);
       this.attackPlayer();
       this.countRound += 1;
     },
     healPlayer() {
-      const healValue = getRandomAttackValue(8, 20);
+      const healValue = getRandomValue(8, 20);
       if (this.playerHealth + healValue > 100) {
         this.playerHealth = 100;
       }
       else {
         this.playerHealth += healValue;
       }
+      this.addLogMessage('player', 'heal', healValue);
       this.attackPlayer();
       this.countRound += 1;
     },
     surrender() {
       this.winner = 'monster';
+    },
+    addLogMessage(who, what, value) {
+      this.logMessageList.unshift({
+        actionBy: who,
+        actionType: what,
+        actionValue: value,
+      });
+    },
+    logMessageByClass(logMessage) {
+      return {'log--player': logMessage.actionBy === 'player', 'log--monster': logMessage.actionBy === 'monster'};
+    },
+    capitalizeFirstLetter(name) {
+      return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
     },
   }
 });

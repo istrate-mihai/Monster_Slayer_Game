93a8567514036ba192cdd5f44aaeca0ab206354d
Istrate-Mihai <i.mihai9960@gmail.com>
2024-11-09 14:24:31 +0200

    Added special attack functionality

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = Vue.createApp({
     return {
       monsterHealth: 100,
       playerHealth: 100,
+      countRound: 0,
     };
   },
   computed: {
@@ -16,16 +17,26 @@ const app = Vue.createApp({
     playerBarStyles() {
       return {width: this.playerHealth + '%'};
     },
+    mayUseSpecialAttack() {
+      return this.countRound % 3 != 0;
+    }
   },
   methods: {
     attackMonster() {
       const attackValue = getRandomAttackValue(5, 12);
       this.monsterHealth -= attackValue;
       this.attackPlayer();
+      this.countRound += 1;
     },
     attackPlayer() {
       const attackValue = getRandomAttackValue(8, 15);
       this.playerHealth -= attackValue;
+    },
+    specialAttackMonster() {
+      const attackValue = getRandomAttackValue(10, 25);
+      this.monsterHealth -= attackValue;
+      this.attackPlayer();
+      this.countRound += 1;
     },
   }
 });
